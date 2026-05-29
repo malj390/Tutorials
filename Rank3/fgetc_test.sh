97b79001b36328d565408144fab34583992217f5
malj390 <miguel.alj390@gamil.com>
2026-05-29 22:36:40 +0200

    29 MAY 26 22:36

--- Rank3/fgetc_test.sh
+++
@@ -1,3 +0,0 @@
-cc -Wall -Wextra -Werror fgetc_test.c -o fgetc_test
-./fgetc_test
-
--- Rank3/ft_scanf.sh
+++
@@ -1,9 +0,0 @@
-cc -Wall -Wextra -Werror scanf.c -o scanf
-cc -Wall -Wextra -Werror ft_scanf.c -o ft_scanf
-
-echo "Scanf original"
-./scanf
-echo " "
-
-echo "ft_scanf"
-./ft_scanf
